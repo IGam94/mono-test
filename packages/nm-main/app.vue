df8b98cbb05059e3716c8e7db27bb2bfcaa014b4
JS <you@example.com>
2023-07-06 10:42:56 +0900

    fix:

--- packages/nm-main/app.vue
+++ packages/nm-main/app.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
-    <Button text="123" color="primary" size="large"></Button>
+    <Button class="font-thin" text="123" color="primary" size="large"></Button>
+    <Tabs title="Tabs01" content="content01" disabled="false"></Tabs>
   </div>
 </template>
 
 <script setup>
 import Button from "@design/system/components/Atom/Button.vue";
+import Tabs from "@design/system/components/Atom/Tabs.vue";
 </script>
-<style></style>
+<style>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
--- packages/nm-main/nuxt.config.ts
+++ packages/nm-main/nuxt.config.ts
@@ -4,7 +4,6 @@ export default defineNuxtConfig({
   postcss: {
     plugins: {
       tailwindcss: {},
-
       autoprefixer: {},
     },
   },
